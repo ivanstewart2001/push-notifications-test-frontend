dd1ad6b04a13de388c7e1266b28e6bd97f1ec4dd
Ivan G Stewart <71293769+ivanstewart2001@users.noreply.github.com>
2024-02-21 02:25:17 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-module.exports = { reactStrictMode: true };
-const withPWA = require("next-pwa");
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    disable: process.env.NODE_ENV === "development",
-    skipWaiting: true,
-  },
+import withPWA from "next-pwa";
+
+export default withPWA({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+  skipWaiting: true,
 });
